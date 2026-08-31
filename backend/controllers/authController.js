2a56cf4ebe5542ff4c922d1dbd3b53772bd94ec3
Henry Chan <henry7410@gmail.com>
2026-08-31 22:59:17 +0800

    SCRUM-85 Return user role in login response

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -7,6 +7,7 @@ const generateToken = (id) => {
     return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '30d' });
 };
 
+// add role
 const registerUser = async (req, res) => {
     const { name, email, password, role } = req.body;
     try {
